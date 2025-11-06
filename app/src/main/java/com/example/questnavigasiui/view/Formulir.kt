1aaa9936663471fbd7d66320b8032eda534677dc
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-06 13:43:14 +0700

    menambah spacer

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -5,6 +5,8 @@ package com.example.questnavigasiui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults.Thickness
@@ -82,6 +84,7 @@ fun FormIsian(
                 label = {Text(text= "Alamat")},
                 onValueChange = {},
             )
+            Spacer(modifier = Modifier.height(30.dp))
         }
     }
 }
