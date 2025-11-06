aad0571b7d70a3160e2e2c3516cf208b13b9acc8
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-06 14:58:04 +0700

    mengubah MainActivity.kt

--- app/src/main/java/com/example/questnavigasiui/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasiui/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigasi.DataApp
 import com.example.questnavigasiui.ui.theme.QuestNavigasiUITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuestNavigasiUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    //kode program bagian yang diedit
+                    DataApp(modifier = Modifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestNavigasiUITheme {
-        Greeting("Android")
     }
 }
