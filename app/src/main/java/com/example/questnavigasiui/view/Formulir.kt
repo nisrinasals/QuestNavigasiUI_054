d005d48ec4a9b26ea31e1bd6bc0d52d6d1bb5844
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-06 13:10:27 +0700

    membuat fun FormIsian pada Formulir.kt

---
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -0,0 +1,12 @@
+package com.example.questnavigasiui.view
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+)
