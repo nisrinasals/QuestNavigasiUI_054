fbf5d6fe8f9e28a3ddeda391044cac4d09d08191
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-06 14:57:32 +0700

    menambah import

--- app/src/main/java/com/example/questnavigasiui/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui/Navigasi.kt
@@ -8,6 +8,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasiui.view.FormIsian
+import com.example.questnavigasiui.view.TampilData
 
 enum class Navigasi {
     Formulirku,
