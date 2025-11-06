db5aa24e6340aa0c9bf162ea250e77f95a1e129b
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-06 12:06:44 +0700

    membuat enum class Navigasi pada Navigasi.kt

--- app/src/main/java/com/example/questnavigasiui/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui/Navigasi.kt
@@ -1,2 +1,12 @@
 package com.example.questnavigasiui
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
