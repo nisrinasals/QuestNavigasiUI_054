4fde741445f4ef9bbdae2f9d0fdbd266885d8630
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-06 13:36:29 +0700

    membuat pilihan untuk jenisK menggunakan row radiobutton + text

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -55,7 +56,18 @@ fun FormIsian(
             HorizontalDivider(modifier = Modifier
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness, color= Color.Red)
-
+            Row{
+                jenisK.forEach {
+                        item ->
+                Row(verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = false,
+                        onClick = {item}
+                    )
+                    Text(text = item)
+                }
+                }
+            }
         }
     }
 }
